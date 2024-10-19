cb5909b5fa33c530ab38f18c76f80fd909b38341
aditya Sahu <43114755+addynoven@users.noreply.github.com>
2024-10-19 22:24:13 +0530

    order change

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <div className="container mx-auto h-full">
                 <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
                     {/* text */}
-                    <div className="text-center xl:text-left">
+                    <div className="text-center xl:text-left order-2 xl:order-none">
                         <span>Software Developer</span>
                         <h1 className="h1 mb-6">
                             Hello I'm <br />{" "}
@@ -43,7 +43,7 @@ const Home = () => {
                         </div>
                     </div>
                     {/* photo */}
-                    <div>
+                    <div className="order-1 xl:order-none mb-8 xl:mb-0">
                         <Photo />
                     </div>
                 </div>
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,4 +1,4 @@
-"use clint";
+"use client";
 
 import { motion } from "framer-motion";
 import Image from "next/image";
@@ -6,15 +6,38 @@ import Image from "next/image";
 const Photo = () => {
     return (
         <div className="w-full h-full relative">
-            <motion.div>
-                <Image
-                    src="/photo.png"
-                    alt="photography"
-                    fill
-                    priority
-                    quality={100}
-                    className="object-contain"
-                />
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{
+                    opacity: 1,
+                    transition: {
+                        delay: 2,
+                        duration: 0.4,
+                        ease: "easeIn",
+                    },
+                }}
+            >
+                <motion.div
+                    initial={{ opacity: 0 }} // initial state
+                    animate={{
+                        opacity: 1,
+                        transition: {
+                            delay: 2.4,
+                            duration: 0.4,
+                            ease: "easeInOut",
+                        },
+                    }}
+                    className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+                >
+                    <Image
+                        src="/photo.png"
+                        alt="photography"
+                        fill
+                        priority
+                        quality={100}
+                        className="object-contain"
+                    />
+                </motion.div>
             </motion.div>
         </div>
     );
