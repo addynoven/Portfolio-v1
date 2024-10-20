62d71a185d7034ae652e799d58a2b7bcaf7062f4
aditya Sahu <43114755+addynoven@users.noreply.github.com>
2024-10-20 21:47:32 +0530

    service page kinda done final check need to be done but done with the most part see

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,89 @@
+"use client";
+import Link from "next/link";
+import { BsArrowDownRight } from "react-icons/bs";
+
+const Services = [
+    {
+        num: "01",
+        title: "Website Development",
+        Description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, quo!",
+        href: "/services/01",
+    },
+    {
+        num: "02",
+        title: "UI/UX Design",
+        Description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, quo!",
+        href: "/services/02",
+    },
+    {
+        num: "03",
+        title: "Game Development",
+        Description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, quo!",
+        href: "/services/03",
+    },
+    {
+        num: "04",
+        title: "App Development",
+        Description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, quo!",
+        href: "/services/04",
+    },
+];
+
+import { motion } from "framer-motion";
+
 import React from "react";
 
-const Services = () => {
-    return <div> Services page</div>;
+const Service = () => {
+    return (
+        <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+            <div className="container mx-auto">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{
+                        opacity: 1,
+                        transition: {
+                            delay: 2.4,
+                            duration: 0.4,
+                            ease: "easeIn",
+                        },
+                    }}
+                    className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+                >
+                    {/* Service */}
+                    {Services.map((service, index) => {
+                        return (
+                            <div
+                                key={service.num}
+                                className=" flex flex-1 flex-col justify-center gap-6 group"
+                            >
+                                {/* {" top "} */}
+                                <div className="w-full flex justify-between items-center">
+                                    <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                                        {service.num}
+                                    </div>
+                                    <Link href={`/services/${service.num}`}>
+                                        <BsArrowDownRight />
+                                    </Link>
+                                </div>
+
+                                {/* {" heading "} */}
+                                <h2>{service.title}</h2>
+
+                                {/* {" description "} */}
+                                <p>{service.Description}</p>
+                                {/* {"Border"} */}
+                                <div className="border-b border-white/20 w-full"></div>
+                            </div>
+                        );
+                    })}
+                </motion.div>
+            </div>
+        </section>
+    );
 };
 
-export default Services;
+export default Service;
---
+++ components/Stats.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import CountUp from "react-countup";
+
+const stats = [
+    { num: 12, Text: "Years of experience" },
+    { num: 20, Text: "Projects Completed" },
+    { num: 5, Text: "Clients" },
+    { num: 500, Text: "Happy Clients" },
+];
+
+const Stats = () => {
+    return (
+        <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+            <div className="container mx-auto">
+                <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+                    {stats.map((stat, index) => {
+                        return (
+                            <div
+                                className="flex flex-1 gap-4 items-center justify-center xl:justify-start"
+                                key={stat.Text}
+                            >
+                                <CountUp
+                                    end={stat.num}
+                                    duration={5}
+                                    delay={2}
+                                    className="text-4xl xl:text-6xl font-extrabold"
+                                />
+
+                                <p
+                                    className={`${
+                                        stat.Text.length < 15
+                                            ? "max-w-[100px]"
+                                            : "max-w-[150px]"
+                                    } leading-snug text-white/80`}
+                                >
+                                    {stat.Text}
+                                </p>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Stats;
