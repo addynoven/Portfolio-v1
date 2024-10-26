3d13497728da5234428f6a661abca94e7f067b92
aditya Sahu <43114755+addynoven@users.noreply.github.com>
2024-10-27 01:07:51 +0530

    header fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,33 +3,33 @@ import { Button } from "./ui/button";
 import Nav from "./Nav";
 import MobileNav from "./MobileNav";
 const Header = () => {
-    return (
-        <header className="py-8 xl:py-12 text-white">
-            <div className="container mx-auto flex items-center justify-between">
-                {/* logo */}
-                <Link href="/">
-                    <h1 className="text-4xl font-semibold">
-                        Luke<span className="text-UserAccent">.</span>
-                    </h1>
-                </Link>
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Neon<span className="text-UserAccent">.</span>
+          </h1>
+        </Link>
 
-                {/* nav */}
-                <div className="hidden xl:flex items-center gap-8">
-                    <Nav />
+        {/* nav */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
 
-                    {/* button */}
-                    <Link href="/contact">
-                        <Button>Hire me</Button>
-                    </Link>
-                </div>
+          {/* button */}
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
 
-                {/* mobile nav */}
-                <div className="flex xl:hidden">
-                    <MobileNav />
-                </div>
-            </div>
-        </header>
-    );
+        {/* mobile nav */}
+        <div className="flex xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
